6c6660b88aedc74ad7ced069b30d908977cfad15
Victor Ribeiro <contato.victoribeiro@gmail.com>
2024-07-23 17:52:08 -0300

    test

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,5 +1,5 @@
 import { Liveblocks } from "@liveblocks/node";
 
 export const liveblocks = new Liveblocks({
-  secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
+  secret: "sk_dev_u0PmWVj75-HUDpi4CTNxlwSg7l5r169Gd12IAq31vrmrf9DImVn26Z6wDMg9Pc5I"
 });
